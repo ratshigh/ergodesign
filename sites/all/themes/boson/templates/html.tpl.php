10c5442e0466459b2bebe3ca1a31a324e5a17be9
Ratshigh <ratshigh@gmail.com>
2015-03-14 13:53:41 -0400

    Second commit

--- sites/all/themes/boson/templates/html.tpl.php
+++ sites/all/themes/boson/templates/html.tpl.php
@@ -3,6 +3,8 @@
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 <head>
 <?php print $head; ?>
+<link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Lato:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title><?php print $head_title; ?></title>
